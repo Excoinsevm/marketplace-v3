d683fb30b3ad6c5a69325b760225b62a0e04d4a0
trigontrade <149458283+trigontrade@users.noreply.github.com>
2024-01-13 17:40:42 +0000

    Update contractAddresses.ts

--- const/contractAddresses.ts
+++ const/contractAddresses.ts
@@ -1,18 +1,12 @@
-/** Replace the values below with the addresses of your smart contracts. */
+export const NETWORK = {
+  name: "Leopardkeen",
+  chainId: 1063590932,
+  rpcUrl: "https://leopardkeen-rpc.eu-north-2.gateway.fm",
+};
 
-// 1. Set up the network your smart contracts are deployed to.
-// First, import the chain from the package, then set the NETWORK variable to the chain.
-import { Mumbai } from "@thirdweb-dev/chains";
-export const NETWORK = Mumbai;
+export const MARKETPLACE_ADDRESS = "0x4e59b44847b379578588920cA78FbF26c0B4956C";
 
-// 2. The address of the marketplace V3 smart contract.
-// Deploy your own: https://thirdweb.com/thirdweb.eth/MarketplaceV3
-export const MARKETPLACE_ADDRESS = "0x83c57ec0dF015eef8401fFb7CB7f66CfA8b6Ff55";
-
-// 3. The address of your NFT collection smart contract.
 export const NFT_COLLECTION_ADDRESS =
-  "0x67b40fC017863743C520cDAec1B367BE1FD721f0";
+  "0x7fe29aC3752Ae64B6e75850f9b739a0E214Ee667";
 
-// (Optional) Set up the URL of where users can view transactions on
-// For example, below, we use Mumbai.polygonscan to view transactions on the Mumbai testnet.
-export const ETHERSCAN_URL = "https://mumbai.polygonscan.com";
+export const ETHERSCAN_URL = "https://leopardkeen-blockscout.eu-north-2.gateway.fm";
