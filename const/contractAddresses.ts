d3c0323fb719e8b280e2ef000ce7d87417d363ce
trigontrade <149458283+trigontrade@users.noreply.github.com>
2024-01-13 20:28:04 +0000

    Update contractAddresses.ts

--- const/contractAddresses.ts
+++ const/contractAddresses.ts
@@ -16,4 +16,4 @@ export const MARKETPLACE_ADDRESS = "0x4e59b44847b379578588920cA78FbF26c0B4956C";
 export const NFT_COLLECTION_ADDRESS =
   "0x7fe29aC3752Ae64B6e75850f9b739a0E214Ee667";
 
-export const ETHERSCAN_URL = "https://leopardkeen-blockscout.eu-north-2.gateway.fm";
+export const ETHERSCAN_URL = "https://www.leopardkeen.xyz/explorer";
