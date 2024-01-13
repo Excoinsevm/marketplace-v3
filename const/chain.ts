af697e16575ee4ceedc130e31a887a1ea94c2bcd
trigontrade <149458283+trigontrade@users.noreply.github.com>
2024-01-13 21:46:08 +0000

    Update chain.ts

--- const/chain.ts
+++ const/chain.ts
@@ -3,7 +3,7 @@ import { Chain } from "@thirdweb-dev/chains";
 const CustomChain: Chain = {
   name: "Leopardkeen",
   chainId: 1063590932,
-  rpc: "https://leopardkeen-rpc.eu-north-2.gateway.fm",
+  rpc: ["https://leopardkeen-rpc.eu-north-2.gateway.fm"], // Change this to an array
   nativeCurrency: {
     name: "ETH",
     symbol: "ETH",
