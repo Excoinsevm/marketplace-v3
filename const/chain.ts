bb58105b1642f8bbc48d6da26d7d7289e4fa634b
trigontrade <149458283+trigontrade@users.noreply.github.com>
2024-01-14 06:55:06 +0000

    Update chain.ts

--- const/chain.ts
+++ const/chain.ts
@@ -1,6 +1,7 @@
 import { Chain } from "@thirdweb-dev/chains";
 
 const CustomChain: Chain = {
+  chain: "Leopardkeen",
   name: "Leopardkeen",
   chainId: 1063590932,
   rpc: ["https://leopardkeen-rpc.eu-north-2.gateway.fm"], // Change this to an array
