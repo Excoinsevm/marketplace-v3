d72218978627b8baaf45941436e6ca509122d320
Excoinsevm <149539033+Excoinsevm@users.noreply.github.com>
2024-01-16 16:48:20 +0000

    Update contractAddresses.ts

--- const/contractAddresses.ts
+++ const/contractAddresses.ts
@@ -4,6 +4,6 @@ export const NETWORK = CustomChain;
 export const MARKETPLACE_ADDRESS = "0x6085E269189E5480041D7519Da67bC8e418fb604";
 
 export const NFT_COLLECTION_ADDRESS =
-  "0x7fe29aC3752Ae64B6e75850f9b739a0E214Ee667";
+  "0x7BfDA75E3Ed7a4DA2d7F004BC031C14BAb0F5d35";
 
-export const ETHERSCAN_URL = "https://www.leopardkeen.xyz/explorer";
+export const ETHERSCAN_URL = "https://explorer.bit-rock.io";
