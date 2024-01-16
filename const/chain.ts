7dc9ca025e6399afc6e81e47a8fefc1c0c56b94a
Excoinsevm <149539033+Excoinsevm@users.noreply.github.com>
2024-01-16 16:50:06 +0000

    Update chain.ts

--- const/chain.ts
+++ const/chain.ts
@@ -1,18 +1,18 @@
 import { Chain } from "@thirdweb-dev/chains";
 
 const CustomChain: Chain = {
-  chain: "Leopardkeen",
-  name: "Leopardkeen",
-  chainId: 1063590932,
-  rpc: ["https://leopardkeen-rpc.eu-north-2.gateway.fm"], // Change this to an array
+  chain: "bitrock",
+  name: "bitrock",
+  chainId: 7171,
+  rpc: ["https://connect.bit-rock.io"], // Change this to an array
   nativeCurrency: {
-    name: "ETH",
-    symbol: "ETH",
+    name: "BROCK",
+    symbol: "BROCK",
     decimals: 18,
   },
   shortName: "custom",
   testnet: false,
-  slug: "leopardkeen",
+  slug: "bitrock",
 };
 
 export default CustomChain;
