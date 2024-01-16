82e72a9915786f9b2b97b7648b6e3200720712a9
Excoinsevm <149539033+Excoinsevm@users.noreply.github.com>
2024-01-16 17:30:13 +0000

    Update contractAddresses.ts

--- const/contractAddresses.ts
+++ const/contractAddresses.ts
@@ -4,6 +4,6 @@ export const NETWORK = CustomChain;
 export const MARKETPLACE_ADDRESS = "0x6085E269189E5480041D7519Da67bC8e418fb604";
 
 export const NFT_COLLECTION_ADDRESS =
-  "0x7BfDA75E3Ed7a4DA2d7F004BC031C14BAb0F5d35";
+  "0xe1D3030656Cb4eA9821f76c708dA1dEA6686C3A7";
 
 export const ETHERSCAN_URL = "https://explorer.bit-rock.io";
