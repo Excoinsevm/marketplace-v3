0cc09b243cc6c85e1295a657e5572e0984bc362f
Excoinsevm <149539033+Excoinsevm@users.noreply.github.com>
2024-04-06 17:39:23 +0100

    Update contractAddresses.ts

--- const/contractAddresses.ts
+++ const/contractAddresses.ts
@@ -1,7 +1,7 @@
 import CustomChain from "../const/chain";
 export const NETWORK = CustomChain;
 
-export const MARKETPLACE_ADDRESS = "0x6085E269189E5480041D7519Da67bC8e418fb604";
+export const MARKETPLACE_ADDRESS = "0x77ABFF44C9f770B03324fE71F686A7268e5D0330";
 
 export const NFT_COLLECTION_ADDRESS =
   "0x5Baa53AfB5b14aA3e7c70D649D56cfa00BA324eE";
